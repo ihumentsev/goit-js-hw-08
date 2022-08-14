67dcf3fd881becca174cac1bd706304febbf1cf0
ihumentsev <a.ihumentsev@gmail.com>
2022-08-14 22:17:47 +0200

    add-homework

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+// -------------
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+// const onPlay = function (data) {
+//   // data is an object containing properties specific to that event
+//   timeupdate: {
+//     duration: 61.857;
+//     percent: 0.049;
+//     seconds: 3.034;
+//   }
+// };
+
+// player.on('play', onPlay);
+// player.on('eventName', function (data) {
+//   // data is an object containing properties specific to that event
+// });
